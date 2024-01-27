3b8bc34e32643f34813d7eca7f76bf745bfb23ac
Akki-net <akashsharma.zak@gmail.com>
2024-01-27 13:09:59 +0530

    host

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
 # ALLOWED_HOSTS = []
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
